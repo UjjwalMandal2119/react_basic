bc5e71f22e762e10b6fa78d8d2fe8f5349dbb81e
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-04 20:08:02 +0530

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+
+let x="ujjwal";
+let array =["user1", "user2", "user3", "user4", "user5"];
+
+let data = "boy";
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+    <Navbar/>
+    {/* display things */}
+    {x}
+    {/* map function using */}
+    {array.map((user)=>{return <h2>{user}</h2>})}
+    {/* conditional rendering */}
+    {data === "boy"?<h1>Boy</h1>: <h1>Girl</h1>}
+    
+   
+    </div>
   )
 }
 

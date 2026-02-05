5a41998a88f478c4cf423b57d2c44aa183440a5d
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-05 08:26:30 +0530

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,25 @@ let array =["user1", "user2", "user3", "user4", "user5"];
 let data = "boy";
 
 const App = () => {
+  let x=0;
+
+  const btnClick= () =>{
+    console.log("clicked");
+    x++;
+    console.log(x);
+  }
   return (
     <div>
     <Navbar/>
+    {x}
     {/* display things */}
     {x}
     {/* map function using */}
     {array.map((user)=>{return <h2>{user}</h2>})}
     {/* conditional rendering */}
     {data === "boy"?<h1>Boy</h1>: <h1>Girl</h1>}
+    {/* event handling */}
+    <button onClick={()=>{btnClick()}}>Click Me</button>
     
    
     </div>
